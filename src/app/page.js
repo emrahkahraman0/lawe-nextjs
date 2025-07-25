25a68211e0d34c35a7cc3ab5de8fd127c7f54dd0
Emrah Kahraman <oempire34@gmail.com>
2025-07-25 17:19:22 +0300

    update-3

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import HomeSlider from "@/templates/HomeSlider";
 import React from "react";
 
 function Home() {
   return (
     <>
       <div>Home Page</div>
+      <HomeSlider />
     </>
   );
 }
--- src/templates/HomeSlider.js
+++ src/templates/HomeSlider.js
@@ -1,9 +1,90 @@
+"use client";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import React from "react";
+import Slider from "react-slick";
+import Image from "next/image";
 
 function HomeSlider() {
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  };
+
   return (
     <>
-      <div>HomeSlider</div>
+      <div id="home_slider">
+        <Slider className="home_slider" {...settings}>
+          <div className="home_slider_item">
+            <Image
+                className="img_fluid"
+                src="/1920x650.png"
+                width={1920}
+                height={650}
+                priority
+                alt="Home Slider 1"
+            />
+            <div className="slider_caption">
+              <h5>Welcome to Lawe</h5>
+              <h6>Most Successful Law Firm</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+              <button>Learn More</button>
+            </div>
+          </div>
+          <div className="home_slider_item">
+            <Image
+                className="img_fluid"
+                src="/1920x650.png"
+                width={1920}
+                height={650}
+                priority
+                alt="Home Slider 1"
+            />
+            <div className="slider_caption">
+              <h5>Welcome to Lawe</h5>
+              <h6>Most Successful Law Firm</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+              <button>Learn More</button>
+            </div>
+          </div>
+          <div className="home_slider_item">
+            <Image
+                className="img_fluid"
+                src="/1920x650.png"
+                width={1920}
+                height={650}
+                priority
+                alt="Home Slider 1"
+            />
+            <div className="slider_caption">
+              <h5>Welcome to Lawe</h5>
+              <h6>Most Successful Law Firm</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+              <button>Learn More</button>
+            </div>
+          </div>
+        </Slider>
+      </div>
+      {/*home_slider*/}
     </>
   );
 }
